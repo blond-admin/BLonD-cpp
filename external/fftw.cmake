c1ab441a4b4911f39c41d907a86ae1047ccd03f4
OlegJakushkin <oleg.jakushkin@gmail.com>
2016-06-30 13:38:24 +0300

    [Windows]: building FFTW successfully

--- external/fftw.cmake
+++ external/fftw.cmake
@@ -1,9 +1,9 @@
 #based on https://github.com/hinerm/ITK/blob/master/CMake/itkExternal_FFTW.cmake
 # Encapsulates building FFTW as an External Project.
-
 if (WITH_FFTW)
     set(fftwLib "fftw")
     GetnerateLibName(${fftwLib})
+    message(STATUS "searching for ${INSTALL_LIB_DIR}${fftwLib}")
     if ((NOT EXISTS ${INSTALL_LIB_DIR}${fftwLib}))
         set(BUILD_FFTW "True")
     endif ()
@@ -13,15 +13,38 @@ if (BUILD_FFTW)
 
     if (WIN32 AND NOT MINGW)
         message(STATUS "Can't build fftw as external project on Windows, downloading official release")
-        message("Can't build fftw as external project on Windows")
-        if (ARCHITECTURE MATCHES "x86")
+        message(STATUS "Can't build fftw as external project on Windows")
+        get_filename_component(VS_BIN_DIR "${CMAKE_LINKER}" DIRECTORY)
+
+        file(DOWNLOAD ftp://ftp.fftw.org/pub/fftw/fftw-3.3-libs-visual-studio-2010.zip ./fftw-prefix/src/project.zip)
+
+        if (ARCHITECTURE MATCHES "x64")
+            set(FFTW_ARCHITECTURE "64")
+        else()
+            set(FFTW_ARCHITECTURE "32")
+            set(ARCHITECTURE  "x86")
+        ENDIF()
+        message("detected architecture set for ARCHITECTURE  ${ARCHITECTURE} for FFTW_ARCHITECTURE  ${FFTW_ARCHITECTURE}")
+
+
             ExternalProject_add(fftw
-                    URL "ftp://ftp.fftw.org/pub/fftw/fftw-3.3.4-dll32.zip"
+                    URL "ftp://ftp.fftw.org/pub/fftw/fftw-3.3.3.zip"
                     CONFIGURE_COMMAND ""
-                    BUILD_COMMAND ""
+                    BUILD_COMMAND
+                    echo %cd%
+                    ${CMAKE_COMMAND} -E tar "xfv" "./project.zip" --format=zip
+                    ${CMAKE_COMMAND} -E rename ./fftw-3.3-libs ./fftw/fftw-3.3-libs
+                    #${VS_BIN_DIR}/lib /machine:x${FFTW_MACHINE_ARCHITECTURE} /def:libfftw3-3.def
+                    ${CMAKE_VS_MSBUILD_COMMAND} ./fftw/fftw-3.3-libs/fftw-3.3-libs.sln /p:PlatformToolset=v140 /p:configuration=Debug /p:platform "${ARCHITECTURE}" /project "libfftw-3.3"
+                    ${CMAKE_VS_MSBUILD_COMMAND} ./fftw/fftw-3.3-libs/fftw-3.3-libs.sln /p:PlatformToolset=v140 /p:configuration=Release /p:platform "${ARCHITECTURE}" /project "libfftw-3.3"
+                        #cmake -E fftw3.h ${INSTALL_DIR}/bin/
+                        #cmake -E libfftw3-3.lib ${INSTALL_DIR}/lib/
+                        #cmake -E fftw3.h ${INSTALL_DIR}/include/
                     INSTALL_COMMAND ""
                     )
-        else ()
+        return() # todo remove!
+
+    else ()
 
         endif ()
     else ()

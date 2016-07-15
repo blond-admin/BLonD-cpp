da40c6c38e01d2b456359c3babcf04fadba06bb2
OlegJakushkin <oleg.jakushkin@gmail.com>
2016-07-15 13:15:14 +0300

    [Merge]: Fixing release configuration build on windows

--- external/benchmark.cmake
+++ external/benchmark.cmake
@@ -14,7 +14,7 @@ if(BUILD_BENCHMARK)
             benchmark-src
             GIT_REPOSITORY https://github.com/google/benchmark
             DOWNLOAD_DIR ${BENCHMARK_ROOT}
-            CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release
+            CMAKE_ARGS
             -DBENCHMARK_ENABLE_LTO=true
             -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
             -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
@@ -34,34 +34,7 @@ if(BUILD_BENCHMARK)
 
         #adding additional step to move release build libraries
         ExternalProject_Add_Step(benchmark-src AFTER_INSTALL
-                COMMAND ${CMAKE_COMMAND} -E copy ${INSTALL_LIB_DIR}/benchmark.lib ${INSTALL_LIB_DIR}/benchmark/Release/benchmark.lib
-                COMMENT "Copy release files into release directory"
-                DEPENDEES Install
-                )
-
-        #building debug version
-        set(BENCHMARK_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/build/gbench-d/")
-        ExternalProject_Add(
-                benchmark-src-d
-                GIT_REPOSITORY https://github.com/google/benchmark
-                DOWNLOAD_DIR ${BENCHMARK_ROOT}
-                CMAKE_ARGS -DCMAKE_BUILD_TYPE=Debug
-                -DBENCHMARK_ENABLE_LTO=true
-                -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-                -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-                -DCMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS_DEBUG}
-                -DCMAKE_CXX_FLAGS_RELEASE=${CMAKE_CXX_FLAGS_RELEASE}
-                -DCMAKE_EXE_LINKER_FLAGS_RELEASE=${CMAKE_EXE_LINKER_FLAGS_RELEASE}
-                -DCMAKE_EXE_LINKER_FLAGS_DEBUG=${CMAKE_EXE_LINKER_FLAGS_DEBUG}
-                TMP_DIR ${BENCHMARK_ROOT}/build/tmp
-                TEST_BEFORE_INSTALL False
-                TEST_AFTER_INSTALL False
-                BINARY_DIR ${BENCHMARK_ROOT}/build
-                INSTALL_DIR ${INSTALL_DIR}
-        )
-
-        ExternalProject_Add_Step(benchmark-src-d AFTER_INSTALL
-                COMMAND ${CMAKE_COMMAND} -E copy ${INSTALL_LIB_DIR}/benchmark.lib ${INSTALL_LIB_DIR}/benchmark/Debug/benchmark.lib
+                COMMAND ${CMAKE_COMMAND} -E copy ${INSTALL_LIB_DIR}/benchmark.lib ${INSTALL_LIB_DIR}/benchmark/$(Configuration)/benchmark.lib
                 COMMAND ${CMAKE_COMMAND} -E remove -f ${INSTALL_LIB_DIR}/benchmark.lib
                 COMMENT "Copy release files into release directory"
                 DEPENDEES Install
--- external/gtest.cmake
+++ external/gtest.cmake
@@ -12,9 +12,10 @@ if(BUILD_GOOGLETEST)
     set(GOOGLETEST_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/build/gtest/")
     ExternalProject_Add(
             googletest-src
+            -DCMAKE_BUILD_TYPE=Release
             GIT_REPOSITORY https://github.com/google/googletest
             DOWNLOAD_DIR ${GOOGLETEST_ROOT}
-            CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release
+            CMAKE_ARGS
             -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG:PATH=DebugLibs
             -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE:PATH=ReleaseLibs
             -Dgtest_force_shared_crt=ON
@@ -38,49 +39,14 @@ if(BUILD_GOOGLETEST)
     if(WIN32 AND NOT MINGW)
         #on windows projects require debug+release libraries
         #adding additional step to move release build libraries
-        ExternalProject_Add_Step(googletest-src AFTER_INSTALL
-                COMMAND ${CMAKE_COMMAND} -E copy ${INSTALL_LIB_DIR}/gtest.lib ${INSTALL_LIB_DIR}/gtest/Release/gtest.lib
-                COMMAND ${CMAKE_COMMAND} -E copy ${INSTALL_LIB_DIR}/gtest_main.lib ${INSTALL_LIB_DIR}/gtest/Release/gtest_main.lib
-                COMMENT "Copy release files into release directory"
-                DEPENDEES Install
-                )
-
-        #building debug version
-        set(GOOGLETEST_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/build/gtest-d/")
-        ExternalProject_Add(
-                googletest-src-dbg
-                GIT_REPOSITORY https://github.com/google/googletest
-                DOWNLOAD_DIR ${GOOGLETEST_ROOT}
-                CMAKE_ARGS -DCMAKE_BUILD_TYPE=Debug
-                -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG:PATH=DebugLibs
-                -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE:PATH=ReleaseLibs
-                -Dgtest_force_shared_crt=ON
-                -DBUILD_GTEST=ON
-                -DBUILD_GMOCK=OFF
-                -Dgtest_disable_pthreads=ON
-                -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-                -DCMAKE_RULE_MESSAGES=OFF
-                -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-                -DCMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS_DEBUG}
-                -DCMAKE_CXX_FLAGS_RELEASE=${CMAKE_CXX_FLAGS_RELEASE}
-                -DCMAKE_EXE_LINKER_FLAGS_RELEASE=${CMAKE_EXE_LINKER_FLAGS_RELEASE}
-                -DCMAKE_EXE_LINKER_FLAGS_DEBUG=${CMAKE_EXE_LINKER_FLAGS_DEBUG}
-                TMP_DIR ${GOOGLETEST_ROOT}/build/tmp
-                TEST_BEFORE_INSTALL False
-                TEST_AFTER_INSTALL False
-                BINARY_DIR ${GOOGLETEST_ROOT}/build
-                INSTALL_DIR ${INSTALL_DIR}
-        )
-
-        ExternalProject_Add_Step(googletest-src-dbg AFTER_INSTALL
-                COMMAND ${CMAKE_COMMAND} -E copy ${INSTALL_LIB_DIR}/gtest.lib ${INSTALL_LIB_DIR}/gtest/Debug/gtest.lib
-                COMMAND ${CMAKE_COMMAND} -E copy ${INSTALL_LIB_DIR}/gtest_main.lib ${INSTALL_LIB_DIR}/gtest/Debug/gtest_main.lib
-                COMMAND ${CMAKE_COMMAND} -E remove -f ${INSTALL_LIB_DIR}/gtest.lib
-                COMMAND ${CMAKE_COMMAND} -E remove -f ${INSTALL_LIB_DIR}/gtest_main.lib
-                COMMENT "Copy release files into release directory"
-                DEPENDEES Install
-                )
-
+            ExternalProject_Add_Step(googletest-src AFTER_INSTALL
+                    COMMAND ${CMAKE_COMMAND} -E copy ${INSTALL_LIB_DIR}/gtest.lib ${INSTALL_LIB_DIR}/gtest/$(Configuration)/gtest.lib
+                    COMMAND ${CMAKE_COMMAND} -E copy ${INSTALL_LIB_DIR}/gtest_main.lib ${INSTALL_LIB_DIR}/gtest/$(Configuration)/gtest_main.lib
+                    COMMAND ${CMAKE_COMMAND} -E remove -f ${INSTALL_LIB_DIR}/gtest.lib
+                    COMMAND ${CMAKE_COMMAND} -E remove -f ${INSTALL_LIB_DIR}/gtest_main.lib
+                    COMMENT "Copy release files into release directory"
+                    DEPENDEES Install
+                    )
         message(STATUS "note: on windows lib builds are seprate for debug and release")
     endif()
 endif()

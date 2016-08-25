5e49c0687819fdb5ac09d6f436484963b65e15b4
OlegJakushkin <oleg.jakushkin@gmail.com>
2016-08-25 14:10:54 +0300

    [Docs]: updated benchmark visualization

--- benchmarks/reports/benchmarks_list.js
+++ benchmarks/reports/benchmarks_list.js
@@ -1,8 +1,4 @@
 benchmarks = [
-	{name: "Sinus (sinus holder is created on each iteration)", ref: "./sin.json"},
-	{name: "Sinus (sinus holder is created before iteration loop)", ref: "./sin_old.json"},
-	{name: "TC1_Accelleration", ref: "./TC1.json"},
-	{name: "TC1_Accelleration_new_OJ", ref: "./TC1_new.json"},
-	{name: "TC1_Accelleration_new_OJ std:sin", ref: "./TC1_new2.json"},
-	{name: "TC1_Accelleration_new_OJ std:sin", ref: "./TC1_atomicMp32.json"},	
+{name: "TC1-3d_data", ref: ["./3d-hybrilit.json", "./3d-windows-i7.json"], axces: ["particles", "turns", "slices"]},
+	{name: "TC1-2d_data", ref: "./TC1.json", axces: ["particles", "turns"]},
 ];

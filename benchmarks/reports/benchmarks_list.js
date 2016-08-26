399fac4dd7de46bc36a3f111138362edcf805b62
OlegJakushkin <oleg.jakushkin@gmail.com>
2016-08-26 05:30:18 +0300

    [Docs]: Benchmark visualization code and samples

--- benchmarks/reports/benchmarks_list.js
+++ benchmarks/reports/benchmarks_list.js
@@ -1,4 +1,4 @@
 benchmarks = [
-{name: "TC1-3d_data", ref: ["./3d-hybrilit.json", "./3d-windows-i7.json"], axces: ["particles", "turns", "slices"]},
-	{name: "TC1-2d_data", ref: "./TC1.json", axces: ["particles", "turns"]},
+    {name: "TC1-3d_data", ref: ["./3d-hybrilit.json", "./3d-windows-i7.json"], axes: ["particles", "turns", "slices"]},
+	{name: "TC1-2d_data", ref: "./TC1.json", axes: ["particles", "turns"]},
 ];

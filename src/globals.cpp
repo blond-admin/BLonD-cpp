0022ee39514d69f2938dd2eb0b1ed6031f51d701
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2016-09-05 11:28:22 +0200

    added documentation

--- src/globals.cpp
+++
@@ -1,9 +0,0 @@
-#include <blond/globals.h>
-Slices* Context::Slice = nullptr;
-GeneralParameters* Context::GP = nullptr;
-Beams* Context::Beam = nullptr;
-RfParameters* Context::RfP = nullptr;
-
-// TODO num of threads is global
-// should it?
-int Context::n_threads = 1;

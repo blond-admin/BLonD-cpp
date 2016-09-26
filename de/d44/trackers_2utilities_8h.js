4e1ffac0f88ca26589e11efee6e6a9a22c9349c7
travis <travis>
2016-09-26 10:14:15 +0000

    documentation update

--- de/d44/trackers_2utilities_8h.js
+++ de/d44/trackers_2utilities_8h.js
@@ -2,7 +2,7 @@ var trackers_2utilities_8h =
 [
     [ "hamiltonian", "de/d44/trackers_2utilities_8h.html#a87fe86bb22d0eb949c7eda2b11e8e83d", null ],
     [ "hamiltonian", "de/d44/trackers_2utilities_8h.html#a92591dac90d475f33947e411e284cfcd", null ],
-    [ "is_in_separatrix", "de/d44/trackers_2utilities_8h.html#a48654bec1980aab27c0929236af271bb", null ],
+    [ "is_in_separatrix", "de/d44/trackers_2utilities_8h.html#a8813a5065ff3ab5a0923846ab24ea265", null ],
     [ "phase_modulo_above_transition", "de/d44/trackers_2utilities_8h.html#a59ef9370e7083327654606faa67366c1", null ],
     [ "phase_modulo_below_transition", "de/d44/trackers_2utilities_8h.html#a7c9aa144919f9eaf5175d7432f40ee5b", null ]
 ];
--- de/da5/utilities_8cpp.js
+++ de/da5/utilities_8cpp.js
@@ -2,5 +2,5 @@ var utilities_8cpp =
 [
     [ "hamiltonian", "de/da5/utilities_8cpp.html#a065b0a231ff4a7264b3bf2672c4f50e6", null ],
     [ "hamiltonian", "de/da5/utilities_8cpp.html#a92201c3f8af91117f18610927136860b", null ],
-    [ "is_in_separatrix", "de/da5/utilities_8cpp.html#adc840b0dc865131c6354ceb43674752f", null ]
+    [ "is_in_separatrix", "de/da5/utilities_8cpp.html#ad87a0030d0468114d36ebfacc62acbe0", null ]
 ];

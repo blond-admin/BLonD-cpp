13e6dd90f82fa789d52b439c810d9325b670a5d9
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2016-10-06 11:01:07 +0200

    restructured Distributions.h

--- include/blond/beams/Distributions.h
+++ include/blond/beams/Distributions.h
@@ -17,8 +17,8 @@
 #include <map>
 #include <random>
 
-inline void longitudinal_bigaussian(ftype sigma_dt, ftype sigma_dE = 0,
-                                    int seed = 0, bool reinsertion = false)
+void longitudinal_bigaussian(ftype sigma_dt, ftype sigma_dE = 0,
+                             int seed = 0, bool reinsertion = false)
 {
     auto GP = Context::GP;
     auto RfP = Context::RfP;
@@ -52,17 +52,12 @@ inline void longitudinal_bigaussian(ftype sigma_dt, ftype sigma_dE = 0,
 
     Beam->sigma_dE = sigma_dE;
     Beam->sigma_dt = sigma_dt;
-    // std::cout << "eta0: " << eta0 << "\n";
-    // std::cout << sigma_dE << "\n";
-    // std::cout << sigma_dt << "\n";
-    // std::cout << (phi_s - phi_RF) / omega_RF << "\n";
+
     if (seed < 0) {
         f_vector_t random;
-        // std::string home = util::GETENV("TEST_FILES");
         util::read_vector_from_file(random, TEST_FILES "/normal_distribution.dat");
         for (uint i = 0; i < Beam->n_macroparticles; ++i) {
             ftype r = random[i % random.size()];
-            // ftype r = distribution(generator);
             if (eta0 > 0)
                 Beam->dt[i] = sigma_dt * r
                               + (phi_s - phi_RF) / omega_RF;
@@ -70,7 +65,6 @@ inline void longitudinal_bigaussian(ftype sigma_dt, ftype sigma_dE = 0,
                 Beam->dt[i] = sigma_dt * r
                               + (phi_s - phi_RF - constant::pi) / omega_RF;
             Beam->dE[i] = sigma_dE * r;
-            // dprintf("beam_dE: %.8lf \n", Beam->dE[i]);
         }
     } else {
         std::default_random_engine generator(seed);
@@ -85,73 +79,12 @@ inline void longitudinal_bigaussian(ftype sigma_dt, ftype sigma_dE = 0,
             r = distribution(generator);
             Beam->dE[i] = sigma_dE * r;
         }
-        // for (uint i = 0; i < Beam->n_macroparticles; ++i)
-        // Beam->dE[i] = sigma_dE * distribution(generator);
     }
 
     // TODO if reinsertion == true
     if (reinsertion) {
         ;
     }
-}
-
-
-
-
-void _matched_from_line_density(ftype beta, ftype energy, ftype charge,
-                                int n_macroparticles, f_vector_t &dt, f_vector_t &dE,
-                                ftype eta_0, ftype t_rev_0,
-                                f_vector_t &potential_well_array,
-                                f_vector_t &time_coord_array,
-                                std::map<std::string, std::string> line_density_opt,
-                                std::string main_harmonic,
-                                std::string plot,
-                                std::string figdir,
-                                std::string half_option,
-                                std::map<std::string, std::string> extraVoltageDict,
-                                int n_iterations_input,
-                                int seed
-                               )
-{
-    // python::initialize();
-    python::import();
-    auto pFunc = python::import("distributions", "matched_from_line_density");
-
-    auto pBeta = python::convert_double(beta);
-    auto pEnergy = python::convert_double(energy);
-    auto pCharge = python::convert_double(charge);
-    auto pNMacroparticles = python::convert_int(n_macroparticles);
-    auto pEta0 = python::convert_double(eta_0);
-    auto pTRev0 = python::convert_double(t_rev_0);
-    auto pMainHarmonic = python::convert_string(main_harmonic);
-    auto pPlot = python::convert_string(plot);
-    auto pFigDir = python::convert_string(figdir);
-    auto pHalfOption = python::convert_string(half_option);
-    auto pNIterationsInput = python::convert_int(n_iterations_input);
-    auto pSeed = python::convert_int(seed);
-    auto pDT = python::convert_double_array(dt.data(), dt.size());
-    auto pDE = python::convert_double_array(dE.data(), dE.size());
-
-    auto pPotentialWell = python::convert_double_array(potential_well_array.data(),
-                          potential_well_array.size());
-
-    auto pTimeCoord = python::convert_double_array(time_coord_array.data(),
-                      time_coord_array.size());
-
-    auto pLineDensityOpt = python::convert_dictionary(line_density_opt);
-    auto pExtraVoltageDict = python::convert_dictionary(extraVoltageDict);
-
-    auto ret = PyObject_CallFunctionObjArgs(pFunc, pBeta, pEnergy,
-                                            pCharge, pNMacroparticles,
-                                            pDT, pDE, pEta0, pTRev0,
-                                            pPotentialWell, pTimeCoord,
-                                            pLineDensityOpt, pMainHarmonic, pPlot,
-                                            pFigDir, pHalfOption, pExtraVoltageDict,
-                                            pNIterationsInput, pSeed, NULL);
-    assert(ret);
-
-
-    // python::finalize();
 }
 
 
@@ -174,89 +107,55 @@ void matched_from_line_density(FullRingAndRf *full_ring,
 
     full_ring->potential_well_generation(0, n_points_potential, 0, 0.4);
 
-
-    _matched_from_line_density(GP->beta[0][0],
-                               GP->energy[0][0],
-                               GP->charge,
-                               Beam->n_macroparticles,
-                               Beam->dt,
-                               Beam->dE,
-                               GP->eta_0[0][0],
-                               GP->t_rev[0],
-                               full_ring->fPotentialWell,
-                               full_ring->fPotentialWellCoordinates,
-                               line_density_opt,
-                               main_harmonic,
-                               plot,
-                               figdir,
-                               half_option,
-                               extraVoltageDict,
-                               n_iterations_input,
-                               seed);
-    // util::dump(Beam->dt, "dt");
-
-}
-
-
-
-
-void _matched_from_distribution_density(ftype beta, ftype energy, ftype charge,
-                                        int n_macroparticles,
-                                        f_vector_t &dt, f_vector_t &dE,
-                                        ftype eta_0, ftype t_rev_0,
-                                        f_vector_t &potential_well_array,
-                                        f_vector_t &time_coord_array,
-                                        std::map<std::string, std::string> distribution_opt,
-                                        std::string main_harmonic,
-                                        int n_iterations_input,
-                                        std::map<std::string, std::string> extraVoltageDict,
-                                        int seed
-                                       )
-{
-
-    // python::initialize();
     python::import();
-
-    auto pFunc = python::import("distributions", "matched_from_distribution_density");
-    auto pBeta = python::convert_double(beta);
-    auto pNMacroparticles = python::convert_int(n_macroparticles);
-    auto pDT = python::convert_double_array(dt.data(), dt.size());
-    auto pEnergy = python::convert_double(energy);
-    auto pCharge = python::convert_double(charge);
-    auto pEta0 = python::convert_double(eta_0);
-    auto pTRev0 = python::convert_double(t_rev_0);
+    auto pFunc = python::import("distributions", "matched_from_line_density");
+    auto pBeta = python::convert_double(GP->beta[0][0]);
+    auto pEnergy = python::convert_double(GP->energy[0][0]);
+    auto pCharge = python::convert_double(GP->charge);
+    auto pNMacroparticles = python::convert_int(Beam->n_macroparticles);
+    auto pEta0 = python::convert_double(GP->eta_0[0][0]);
+    auto pTRev0 = python::convert_double(GP->t_rev[0]);
     auto pMainHarmonic = python::convert_string(main_harmonic);
+    auto pPlot = python::convert_string(plot);
+    auto pFigDir = python::convert_string(figdir);
+    auto pHalfOption = python::convert_string(half_option);
     auto pNIterationsInput = python::convert_int(n_iterations_input);
     auto pSeed = python::convert_int(seed);
-    auto pDE = python::convert_double_array(dE.data(), dE.size());
+    auto pDT = python::convert_double_array(Beam->dt.data(), Beam->dt.size());
+    auto pDE = python::convert_double_array(Beam->dE.data(), Beam->dE.size());
 
-    auto pPotentialWell = python::convert_double_array(potential_well_array.data(),
-                          potential_well_array.size());
+    auto pPotentialWell = python::convert_double_array(
+                              full_ring->fPotentialWell.data(),
+                              full_ring->fPotentialWell.size());
 
-    auto pTimeCoord = python::convert_double_array(time_coord_array.data(),
-                      time_coord_array.size());
+    auto pTimeCoord = python::convert_double_array(
+                          full_ring->fPotentialWellCoordinates.data(),
+                          full_ring->fPotentialWellCoordinates.size());
 
-    auto pDistributionOpt = python::convert_dictionary(distribution_opt);
+    auto pLineDensityOpt = python::convert_dictionary(line_density_opt);
     auto pExtraVoltageDict = python::convert_dictionary(extraVoltageDict);
 
     auto ret = PyObject_CallFunctionObjArgs(pFunc, pBeta, pEnergy,
                                             pCharge, pNMacroparticles,
                                             pDT, pDE, pEta0, pTRev0,
                                             pPotentialWell, pTimeCoord,
-                                            pDistributionOpt, pMainHarmonic,
-                                            pNIterationsInput,
+                                            pLineDensityOpt, pMainHarmonic,
+                                            pPlot, pFigDir, pHalfOption,
                                             pExtraVoltageDict,
-                                            pSeed, NULL);
-    assert(ret);
+                                            pNIterationsInput, pSeed, NULL);
 
-    // python::finalize();
-
+    if (!ret) {
+        std::cerr << "[matched_from_line_density] An error occured while "
+                  << "executing python code\n";
+        exit(-1);
+    }
 
 }
 
 
 void matched_from_distribution_density(FullRingAndRf *full_ring,
-                                       std::map<std::string, std::string> distribution_opt,
+                                       std::map<std::string,
+                                       std::string> distribution_opt,
                                        std::string main_harmonic = "lowest_freq",
                                        int n_iterations_input = 1,
                                        std::map<std::string, std::string> extraVoltageDict =
@@ -271,23 +170,45 @@ void matched_from_distribution_density(FullRingAndRf *full_ring,
 
     full_ring->potential_well_generation(0, n_points_potential, 0, 0.4);
 
+    python::import();
+    auto pFunc = python::import("distributions",
+                                "matched_from_distribution_density");
+    auto pBeta = python::convert_double(GP->beta[0][0]);
+    auto pNMacroparticles = python::convert_int(Beam->n_macroparticles);
+    auto pDT = python::convert_double_array(Beam->dt.data(), Beam->dt.size());
+    auto pDE = python::convert_double_array(Beam->dE.data(), Beam->dE.size());
+    auto pEnergy = python::convert_double(GP->energy[0][0]);
+    auto pCharge = python::convert_double(GP->charge);
+    auto pEta0 = python::convert_double(GP->eta_0[0][0]);
+    auto pTRev0 = python::convert_double(GP->t_rev[0]);
+    auto pMainHarmonic = python::convert_string(main_harmonic);
+    auto pNIterationsInput = python::convert_int(n_iterations_input);
+    auto pSeed = python::convert_int(seed);
 
-    _matched_from_distribution_density(GP->beta[0][0],
-                                       GP->energy[0][0],
-                                       GP->charge,
-                                       Beam->n_macroparticles,
-                                       Beam->dt,
-                                       Beam->dE,
-                                       GP->eta_0[0][0],
-                                       GP->t_rev[0],
-                                       full_ring->fPotentialWell,
-                                       full_ring->fPotentialWellCoordinates,
-                                       distribution_opt,
-                                       main_harmonic,
-                                       n_iterations_input,
-                                       extraVoltageDict,
-                                       seed);
-    // util::dump(Beam->dt, "dt");
+    auto pPotentialWell = python::convert_double_array(
+                              full_ring->fPotentialWell.data(),
+                              full_ring->fPotentialWell.size());
+
+    auto pTimeCoord = python::convert_double_array(
+                          full_ring->fPotentialWellCoordinates.data(),
+                          full_ring->fPotentialWellCoordinates.size());
+
+    auto pDistributionOpt = python::convert_dictionary(distribution_opt);
+    auto pExtraVoltageDict = python::convert_dictionary(extraVoltageDict);
+
+    auto ret = PyObject_CallFunctionObjArgs(pFunc, pBeta, pEnergy,
+                                            pCharge, pNMacroparticles,
+                                            pDT, pDE, pEta0, pTRev0,
+                                            pPotentialWell, pTimeCoord,
+                                            pDistributionOpt, pMainHarmonic,
+                                            pNIterationsInput,
+                                            pExtraVoltageDict,
+                                            pSeed, NULL);
+    if (!ret) {
+        std::cerr << "[matched_from_line_density] An error occured while "
+                  << "executing python code\n";
+        exit(-1);
+    }
 
 }
 

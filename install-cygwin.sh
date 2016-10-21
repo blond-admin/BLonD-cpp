086a699eeb79197f4e1c4b8924c0fb27affa4040
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2016-10-21 14:50:35 +0200

    CMakeLists is ready for Cygwin on Windows installation, install-cygwin.sh part2 is ready to be tested

---
+++ install-cygwin.sh
@@ -0,0 +1,345 @@
+#!/bin/bash
+
+verlte() {
+    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
+
+verlt() {
+    [ "$1" = "$2" ] && return 1 || verlte $1 $2
+}
+
+LIB="/lib"
+INCLUDE="/usr/include"
+BLOND_HOME="$(pwd)"
+EXTERNAL="${BLOND_HOME}/external"
+INSTALL="${EXTERNAL}/install"
+log="${EXTERNAL}/log.out"
+
+mkdir -p ${EXTERNAL}/tmp
+mkdir -p ${INSTALL}/lib
+mkdir -p ${INSTALL}/include
+echo > ${log}
+
+
+echo -e "---- BLonD++ on Windows auto-configuration script\n"
+
+echo -e "---- Start of Part1: Cygwin packages installation\n\n"
+
+
+echo -e "---- End of Part1: Cygwin packages installation\n\n"
+
+
+
+
+echo -e "---- Start of Part2: External depedencies installation\n\n"
+
+
+INSTALL_FFTW=true
+INSTALL_GTEST=true
+INSTALL_PYTHON=true
+INSTALL_HDF5=true
+INSTALL_GBENCH=false
+SKIP_QUESTIONS=false
+
+while getopts ":f:t:p:h:b:s" opt; do
+    case $opt in 
+        f)
+            INSTALL_FFTW=$OPTARG
+            ;;
+        t)
+            INSTALL_GTEST=$OPTARG
+            ;;
+        p)
+            INSTALL_PYTHON=$OPTARG
+            ;;
+        h)
+            INSTALL_HDF5=$OPTARG
+            ;;
+        b)
+            INSTALL_GBENCH=$OPTARG
+            # echo "-b specified with arg $OPTARG"
+            ;;
+        s) SKIP_QUESTIONS=true
+            ;;
+        :)
+            echo -e "Option -$OPTARG requires and argument"
+            ;;
+        \?)
+            echo -e "Invalid option -$OPTARG"
+            ;;
+    esac
+done
+
+
+# -----------------
+# fftw installation
+# -----------------
+
+
+if [ -e ${INSTALL}/include/fftw3.h ] && [ -e ${INSTALL}/lib/libfftw3.dll.a ]; then
+    echo -e "\n\n---- Looks like fftw3 is already installed,"
+    if [ "${SKIP_QUESTIONS}" = "true" ] ; then
+        echo -e "---- fftw3 installation will be skipped"
+        INSTALL_FFTW=false
+    else
+        echo -e "---- are you sure you want to reinstall it?"
+        select yn in "Yes" "No"; do
+            case $yn in
+                Yes ) INSTALL_FFTW=true; break;;
+                No ) INSTALL_FFTW=false; break;;
+            esac
+        done
+    fi
+fi
+
+
+if [ "${INSTALL_FFTW}" = "true" ] ; then
+    echo -e "\n\n---- Installing fftw3"
+    cp ${LIB}/libfftw3.dll.a ${INSTALL}/lib/
+    cp ${INCLUDE}/fftw3.h ${INSTALL}/include/
+
+    if [ -e ${INSTALL}/include/fftw3.h ] && [ -e ${INSTALL}/lib/libfftw3.dll.a ]; then
+        echo -e "---- fftw3 has been installed successfully\n\n"
+    else
+        echo -e "---- fftw3 has failed to install successfully"
+        echo -e "---- You will have to manually install this library"
+        echo -e "---- into directory ${BLOND_HOME}/external/install\n\n"
+
+    fi
+    echo -e "---- Installation of fftw3 is completed\n\n"
+fi
+
+# -----------------------
+# end of fftw intallation
+# -----------------------
+
+
+cd ${BLOND_HOME}
+
+
+# -----------------
+# HDF5 installation
+# -----------------
+
+if [ -e ${INSTALL}/include/hdf5.h ] && \
+    [ -e ${INSTALL}/lib/libhdf5.dll.a ] && \
+    [ -e ${INSTALL}/lib/libhdf5_cpp.dll.a ] && \
+    [ -e ${INSTALL}/lib/libhdf5_hl.dll.a ] && \
+    [ -e ${INSTALL}/lib/libhdf5_hl_cpp.dll.a ]
+then
+    echo -e "\n\n---- Looks like HDF5 is already installed,"
+    if [ "${SKIP_QUESTIONS}" = "true" ] ; then
+        echo -e "---- HDF5 installation will be skipped"
+        INSTALL_HDF5=false
+    else
+        echo -e "---- are you sure you want to reinstall it?"
+        select yn in "Yes" "No"; do
+            case $yn in
+                Yes ) INSTALL_HDF5=true; break;;
+                No ) INSTALL_HDF5=false; break;;
+            esac
+        done
+    fi
+fi
+
+
+if [ "${INSTALL_HDF5}" = "true" ]; then
+    echo -e "\n\n---- Installing HDF5"
+    cp ${LIB}/libhdf5.dll.a ${INSTALL}/lib/
+    cp ${LIB}/libhdf5_cpp.dll.a ${INSTALL}/lib/
+    cp ${LIB}/libhdf5_hl.dll.a ${INSTALL}/lib/
+    cp ${LIB}/libhdf5_hl_cpp.dll.a ${INSTALL}/lib/
+    cp ${INCLUDE}/H5*.h ${INSTALL}/include/
+    cp ${INCLUDE}/hdf5.h ${INSTALL}/include/
+    cp ${INCLUDE}/hdf5_hl.h ${INSTALL}/include/
+
+    if [ -e ${INSTALL}/include/hdf5.h ] && \
+        [ -e ${INSTALL}/lib/libhdf5.dll.a ] && \
+        [ -e ${INSTALL}/lib/libhdf5_cpp.dll.a ] && \
+        [ -e ${INSTALL}/lib/libhdf5_hl.dll.a ] && \
+        [ -e ${INSTALL}/lib/libhdf5_hl_cpp.dll.a ]
+    then
+        echo -e "---- HDF5 has been installed successfully\n\n"
+    else
+        echo -e "---- HDF5 has failed to install successfully"
+        echo -e "---- You will have to manually install this library"
+        echo -e "---- into directory ${BLOND_HOME}/external/install\n\n"
+    fi
+    echo -e "---- Installation of HDF5 is completed\n\n"
+fi
+
+# -----------------------
+# end of HDF5 intallation
+# -----------------------
+
+
+cd ${BLOND_HOME}
+
+# -----------------------
+# googletest installation
+# -----------------------
+
+if [ -e ${INSTALL}/include/gtest/gtest.h ] && [ -e ${INSTALL}/lib/libgtest.dll.a ] \
+    && [ -e ${INSTALL}/lib/libgtest_main.a ]; then
+    echo -e "\n\n---- Looks like googletest is already installed,"
+    if [ "${SKIP_QUESTIONS}" = "true" ] ; then
+        echo -e "---- googletest installation will be skipped"
+        INSTALL_GTEST=false
+    else
+        echo -e "---- are you sure you want to reinstall it?"
+        select yn in "Yes" "No"; do
+            case $yn in
+                Yes ) INSTALL_GTEST=true; break;;
+                No ) INSTALL_GTEST=false; break;;
+            esac
+        done
+    fi
+fi
+
+if [ "${INSTALL_GTEST}" = "true" ] ; then
+
+    echo -e "\n\n---- Installing googletest"
+
+    git clone --branch=master https://github.com/google/googletest.git ${EXTERNAL}/tmp/googletest
+    cd ${EXTERNAL}/tmp/googletest/googletest
+    cp -r include/* "${INSTALL}/include/"
+    mkdir -p build &>> $log
+    cd build
+    cmake .. &>> $log
+    make &>> $log
+    cp libgtest_main.a "${INSTALL}/lib/libgtest_main.a"
+    cp libgtest.a "${INSTALL}/lib/libgtest.dll.a"
+
+    cd ${BLOND_HOME}
+
+    if [ -e ${INSTALL}/include/gtest/gtest.h ] && [ -e ${INSTALL}/lib/libgtest.dll.a ] \
+        && [ -e ${INSTALL}/lib/libgtest_main.a ]; then
+        echo -e "---- Googletest has been installed successfully\n\n"
+    else
+        echo -e "---- Googletest has failed to install successfully"
+        echo -e "---- You will have to manually install this library"
+        echo -e "---- into directory ${BLOND_HOME}/external/install\n\n"
+    fi
+    echo -e "---- Installation of googletest is completed\n\n"
+fi
+
+# ------------------------------
+# end of googletest installation
+# ------------------------------
+
+cd ${BLOND_HOME}
+
+
+
+# -----------------------
+# googlebench installation
+# -----------------------
+
+
+# if [ "${INSTALL_GBENCH}" = "true" ] ; then
+
+#    echo -e "\n\n---- Installing googlebench"
+
+#    git clone https://github.com/google/benchmark.git ${EXTERNAL}/tmp/googlebench
+#    cd ${EXTERNAL}/tmp/googlebench/
+#    cp -r include/* "${INSTALL}/include/"
+#    mkdir -p build &>> $log
+#    cd build
+#    CC=gcc CXX=g++ cmake -DCMAKE_BUILD_TYPE=Release \
+#                         -DBENCHMARK_CXX_LINKER_FLAGS="-lrt" \
+#                         .. &>> $log
+#    make &>> $log
+#    cp src/*.a "${INSTALL}/lib"
+
+#    cd ${BLOND_HOME}
+
+#    if [ -e ${INSTALL}/include/benchmark/benchmark.h ] && [ -e ${INSTALL}/lib/libbenchmark.a ]; then
+#       echo -e "---- googlebench has been installed successfully\n\n"
+#    else
+#       echo -e "---- googlebench has failed to install successfully"
+#       echo -e "---- You will have to manually install this library"
+#       echo -e "---- into directory ${BLOND_HOME}/external/install\n\n"
+#    fi
+#    echo -e "---- Installation of googlebench is completed\n\n"
+# fi
+
+# ------------------------------
+# end of googlebench installation
+# ------------------------------
+
+
+cd ${BLOND_HOME}
+
+# -------------------
+# Python installation
+# -------------------
+
+if [ -e ${INSTALL}/include/python2.7/Python.h ] && [ -e ${INSTALL}/lib/libpython2.7.dll.a ]; then
+    echo -e "\n\n---- Looks like Python2.7 is already installed,"
+    if [ "${SKIP_QUESTIONS}" = "true" ] ; then
+        echo -e "---- Python2.7 installation will be skipped"
+        INSTALL_PYTHON=false
+    else
+        echo -e "---- are you sure you want to reinstall it?"
+        select yn in "Yes" "No"; do
+            case $yn in
+                Yes ) INSTALL_PYTHON=true; break;;
+                No ) INSTALL_PYTHON=false; break;;
+            esac
+        done
+    fi
+fi
+
+if [ "${INSTALL_PYTHON}" = "true" ] ; then
+    echo -e "\n\n---- Installing Python2.7\n\n"
+    cp ${LIB}/libpython2.7.dll.a ${INSTALL}/lib/
+    cp -r ${LIB}/python2.7 ${INSTALL}/lib
+    cp -r ${INCLUDE}/python2.7 ${INSTALL}/include
+
+    cd ${BLOND_HOME}
+
+    if [ -e ${INSTALL}/include/python2.7/Python.h ] && [ -e ${INSTALL}/lib/libpython2.7.dll.a ]; then
+        echo -e "---- Python has been installed successfully\n"
+        python -m ensurepip
+        echo -e "---- Pip has been installed successfully\n\n"
+    else
+        echo -e "\n\n---- Python has failed to install successfully"
+        echo -e "---- You will have to manually install this library"
+        echo -e "---- into directory ${BLOND_HOME}/external/install\n\n"
+    fi
+
+fi
+
+
+
+# --------------------------
+# end of Python installation
+# --------------------------
+
+
+# -----------------------
+# Python external modules installation
+# -----------------------
+PIP_INSTALLED=$(python -m pip -V | echo $?)
+if [ "$PIP_INSTALLED" == "1" ]; then
+    echo -e "\n\n---- PIP is needed in order to install the required python modules\n\n"
+else
+    echo -e "\n\n---- Installing Python's external modules..."
+    python -m pip install -r ${EXTERNAL}/python-packages.txt 2>> $log
+    export PYTHONPATH="${BLOND_HOME}/python:$PYTHONPATH"
+    echo -e "\n\n---- Python's external modules have been installed successfully\n\n"
+fi
+# ----------------------------------
+# end of Python Modules installation
+# ----------------------------------
+
+
+
+
+echo -e "\n\n---- Exteranl depedencies installation procedure completed"
+echo -e "---- You may now try to build the project :)"
+echo -e "---- You can consult the ${log} file for any errors\n"
+
+echo -e "---- End of Part2: External depedencies installation\n\n"
+
+rm -rf ${EXTERNAL}/tmp &> /dev/null

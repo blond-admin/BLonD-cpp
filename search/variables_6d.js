aef1196e411423278b58751d6ea7b4ed5f0d689f
travis <travis>
2016-12-05 13:19:08 +0000

    documentation update

--- search/variables_6d.js
+++ search/variables_6d.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['m_5fe',['m_e',['../da/db5/namespaceconstant.html#a76d454fe8f8779dc3298e881b11b5c56',1,'constant']]],
   ['m_5fp',['m_p',['../da/db5/namespaceconstant.html#a50b3384a5f9abb737cb228ba2d781381',1,'constant']]],
-  ['mass',['mass',['../d9/d7c/classBeams.html#a46307c6334da44881bd56169d5a30500',1,'Beams::mass()'],['../d1/dfc/classGeneralParameters.html#a7947471d0866a47f0dc3202c63a432bd',1,'GeneralParameters::mass()']]],
-  ['mass2',['mass2',['../d1/dfc/classGeneralParameters.html#a2262f519b3707a6d5ba5bbc6f3c949fa',1,'GeneralParameters']]],
+  ['mass',['mass',['../d9/d7c/classBeams.html#a46307c6334da44881bd56169d5a30500',1,'Beams::mass()'],['../d1/dfc/classGeneralParameters.html#ace115ff39c9796d1556208ce5455af20',1,'GeneralParameters::mass()']]],
+  ['mass2',['mass2',['../d1/dfc/classGeneralParameters.html#acd923840d5aca93729ca182cf7afe4ab',1,'GeneralParameters']]],
   ['max_5fline_5fin_5fblock',['max_line_in_block',['../de/dc9/classoption_1_1PrintUsageImplementation_1_1LinePartIterator.html#ac76e736568519801a42ddb9069f0a6a7',1,'option::PrintUsageImplementation::LinePartIterator']]],
   ['maxlogf',['MAXLOGF',['../d1/d47/namespacevdt_1_1details.html#af1824fbaba646af63fbd602058fef7a2',1,'vdt::details']]],
   ['maxnumf',['MAXNUMF',['../d1/d47/namespacevdt_1_1details.html#ac550fc2478e5a3510e59f4f9ae94b316',1,'vdt::details']]],
--- search/variables_72.js
+++ search/variables_72.js
@@ -6,9 +6,9 @@ var searchData=
   ['rf_5fkick_5finterp',['rf_kick_interp',['../de/da1/classRingAndRfSection.html#a799fd4f05f1593ed68b5daf087e7b469',1,'RingAndRfSection']]],
   ['rfnoise',['RFnoise',['../d6/d87/classPhaseLoop.html#ae7403a2c2ca509e90939edaf3ea6377b',1,'PhaseLoop']]],
   ['rfp',['rfp',['../dc/d45/classSlices.html#a9a83186373a5a3865b66324c934db1d7',1,'Slices::rfp()'],['../de/da1/classRingAndRfSection.html#a9fef0f50fcda4c9fb84ff86fc143c764',1,'RingAndRfSection::rfp()'],['../d6/d7a/structContext.html#a48db4fc5c2a199a0c1685dc11f5f0a6f',1,'Context::RfP()']]],
-  ['ring_5fcircumference',['ring_circumference',['../d1/dfc/classGeneralParameters.html#aa66806ebe9d5eb2f8e3ea334cb60b37a',1,'GeneralParameters::ring_circumference()'],['../da/d49/classRfParameters.html#ab28bc32ae2d22ffb23a56b0c27a59971',1,'RfParameters::ring_circumference()']]],
+  ['ring_5fcircumference',['ring_circumference',['../d1/dfc/classGeneralParameters.html#a7a1674550ca03836c7ce59ff46ad2766',1,'GeneralParameters::ring_circumference()'],['../da/d49/classRfParameters.html#ab28bc32ae2d22ffb23a56b0c27a59971',1,'RfParameters::ring_circumference()']]],
   ['ring_5flength',['ring_length',['../d1/dfc/classGeneralParameters.html#a1a1681992a7947522ab831656ff41f1e',1,'GeneralParameters']]],
-  ['ring_5fradius',['ring_radius',['../d1/dfc/classGeneralParameters.html#a6af0a75ba89dd912483ef5fb210b473a',1,'GeneralParameters']]],
+  ['ring_5fradius',['ring_radius',['../d1/dfc/classGeneralParameters.html#a3ffce326e62a227c0649e7acfd952d27',1,'GeneralParameters']]],
   ['rowdesc',['rowdesc',['../de/dc9/classoption_1_1PrintUsageImplementation_1_1LinePartIterator.html#a16319411b47f613f2277db4e9a29dbb5',1,'option::PrintUsageImplementation::LinePartIterator']]],
   ['rowstart',['rowstart',['../de/dc9/classoption_1_1PrintUsageImplementation_1_1LinePartIterator.html#a5e82df3342ea1dee5c6c59856fdd24d4',1,'option::PrintUsageImplementation::LinePartIterator']]]
 ];

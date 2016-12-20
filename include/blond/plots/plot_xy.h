b093472bbaf5b7726e5ec7a2a02087411583a487
Alexandre Lasheen <alasheen@lxplus080.cern.ch>
2016-12-20 12:28:14 +0100

    Added plot_xy function

---
+++ include/blond/plots/plot_xy.h
@@ -0,0 +1,17 @@
+/*
+ * plot_xy.h
+ *
+ *  Created on: Dec 20, 2016
+ *      Author: alasheen
+ */
+
+#ifndef INCLUDE_BLOND_PLOTS_PLOT_XY_H_
+#define INCLUDE_BLOND_PLOTS_PLOT_XY_H_
+
+#include <blond/utilities.h>
+
+int plot_xy(f_vector_t xArray, f_vector_t yArray, std::string figureName, std::string dirName, std::string saveFigName, double xMax=0);
+
+
+
+#endif /* INCLUDE_BLOND_PLOTS_PLOT_XY_H_ */
